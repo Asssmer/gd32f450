3475eb4ff1a55aa55aabebeb82e180c977c4e061
Asssmer <47934306+Asssmer@users.noreply.github.com>
2023-12-19 11:08:05 +0800

    ver1_0
    befor_rtos

--- gd32F450_breath/main.c
+++ gd32F450_breath/main.c
@@ -2,28 +2,52 @@
 
 // SystemCoreClock
 
-
 int main(void)
 {
         init_454();
-        extern volatile uint8_t MOTOR_received_frame[MOTOR_FRAME_SIZE];
+        // 电机
         extern volatile MotorStatus motor_status;
-        motor_control(0);
+        // ADC
+        extern volatile uint16_t adc_values_454[ADC_CHANNEL_COUNT];
+        // 温度
+        int16_t temperature_P7;
+        int16_t temperature_P9;
+        // 压力
+        float pressure_P10;
+        float pressure_P11;
+        float pressure_P12;
+        // 流量
+        float flow_P13;
+        float flow_P14;
+        float flow_P15;
+
+        // motor_control(0);
+        LED1(ON);
+        LED2(ON);
+        LED3(ON);
 
         uint8_t data;
         uint16_t motor_control_data;
-        char input_line[100];
+        printf("Start!/n");
         while (1)
         {
-                if (scanf("%hu", &motor_control_data) == 1)
-                {
-                        motor_control(motor_control_data);
-                        gpio_bit_toggle(GPIOG, GPIO_PIN_6);
-                }
-                else
-                {
-                }
+                // if (scanf("%hu", &motor_control_data) == 1)
+                // {
+                //         motor_control(motor_control_data);
+                //         gpio_bit_toggle(GPIOG, GPIO_PIN_6);
+                // }
+                // else
+                // {
+                // }
+                temperature_P7 = P7_get();
+                pressure_P10 = P10_get();
+                pressure_P11 = P11_get();
+                // pressure_P12 = P13_get();
+                flow_P13 = P13_get();
+                flow_P14 = P14_get();
+                // flow_P15 = P15_get();
+                gpio_bit_toggle(GPIOG, GPIO_PIN_6);
+                delay_ms_454(1000);
         }
-        // delay_ms_454(1000);
         return 0;
 }
